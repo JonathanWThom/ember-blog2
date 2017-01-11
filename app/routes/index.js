fffe9a9e85de2f61d1f1ea8f601791f805b17d6f
Jonathan Thom <jonathan.thom1990@gmail.com>
2017-01-11 13:52:02 -0800

    Add comments to posts function

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,12 +1,6 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  // model() {
-  //   this.store.findAll('post').then(function(posts) {
-  //     return posts.objectAt(1);
-  //   });
-  // }
-
   model() {
     return this.store.findAll('post');
   }
